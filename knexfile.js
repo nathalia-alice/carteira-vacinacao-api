97a65b2264b22910ba77d2161cbd838f6a88789f
nathalia-alice <nathalia.alice.silv@gmail.com>
2020-06-09 21:45:33 -0300

    Atualizando knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,6 @@
 
 module.exports = {
 
-  development: {
-    client: process.env.KNEX_CLIENT,
-    connection: {
-      host : process.env.KNEX_DEV_HOST,
-      database: process.env.KNEX_DEV_DATABASE,
-      user:     process.env.KNEX_DEV_USER,
-      password: process.env.KNEX_DEV_PASSWORD
-    }
-  },
-
   production: {
     client: process.env.KNEX_CLIENT,
     connection: {
