c8a709d1a8751f00731faccdaa1e77b6b1567f6d
nathalia-alice <nathalia.alice.silv@gmail.com>
2020-06-14 22:08:49 -0300

    Atualiza controle de vacinas por usuário

--- src/controllers/VaccinesUserController.js
+++ src/controllers/VaccinesUserController.js
@@ -23,15 +23,44 @@ module.exports = {
     },
 
     async create(request, response) {
-        const { id_vaccine, id_user, date } = request.body;
+        var date = request.body.date;
+
+        const consultVaccine = await connection('vaccines')
+        .where('name_vaccine', request.body.id_vaccine);
+
+        const consultUser = await connection('users')
+        .where('doc', request.body.id_user);
+
+        var id_vaccine = consultVaccine[0].id;
+        var id_user = consultUser[0].id;
 
         await connection('vaccinesxuser').insert({
-          id_vaccine,
+            id_vaccine,
           id_user,
           date
         });
 
-        return response.json({ id_vaccine, id_user, date });
+        return response.json({ date });
+    },
+
+    async index(request, response) {
+        const userId = request.userId;
+        const type = request.type;
+
+        var vaccinesUser;
+
+        if(type === "cidadao"){ 
+            vaccinesUser = await connection('vaccinesxuser').select('name', 'id_vaccine', 'id_user', 'name_vaccine', 'description', 'date').
+            innerJoin('users', 'users.id', 'vaccinesxuser.id_user').
+            innerJoin('vaccines', 'vaccinesxuser.id_vaccine', 'vaccines.id')
+            .where('users.id', '=', userId)
+        }else{
+            vaccinesUser = await connection('vaccinesxuser').select('name', 'id_vaccine', 'id_user', 'name_vaccine', 'description', 'date').
+            innerJoin('users', 'users.id', 'vaccinesxuser.id_user').
+            innerJoin('vaccines', 'vaccinesxuser.id_vaccine', 'vaccines.id');
+        }
+ 
+        return response.json(vaccinesUser);
     },
 
 }
